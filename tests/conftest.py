c7bae223f01ddede1bb8eaf3df9edd773827285b
Vladimir Lebedev <lvv1985@gmail.com>
2025-03-04 23:07:04 +0700

    add test invalid_types for get_mask_card_number

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+from tarfile import TruncatedHeaderError
 
+import pytest
 
 @pytest.fixture
 def valid_card_number() -> str:
@@ -12,4 +14,10 @@ def valid_card_number_int() -> int:
     return 1234567890123456
 
 
-
+@pytest.mark.paramitrize("invalid type",
+                         [1.5, #float
+                         [1, 2, 3, 4, 5, 6, 7], #list
+                         {"one":1, "two":2}, #dictionary
+                         (1, 2, 3, 4), #tuple
+                         True #bool
+                         ])
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -11,14 +11,22 @@ def test_get_mask_card_number_with_valid_str(valid_card_number:str) -> None:
     result = get_mask_card_number(valid_card_number)
     assert result == expected
 
+
 def test_get_mask_card_number_with_valid_int(valid_card_number_int:int) -> None:
     """Тест с валидным челочисленным номером карты"""
     expected = '1234 56** **** 3456'
     result = get_mask_card_number(valid_card_number_int)
     assert result == expected
 
+
 def test_get_mask_card_number_with_none() -> None:
-    """Тест с валидным челочисленным номером карты"""
+    """Тест с None в качестве ИД"""
     expected = None
-    result = get_mask_card_number(valid_card_number_int)
-    assert result == expected
+    result = get_mask_card_number() -> None:
+    assert result is None
+
+
+def test_get_mask_card_number_with_invalid_types(invalid_type: Any) -> None:
+    """Тест с недопустимыми типами исходных данных"""
+    result = get_mask_card_number(invalid_type)
+    assert result is None
