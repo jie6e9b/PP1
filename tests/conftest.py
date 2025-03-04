006847a50d8ac7bdba57e977cb1dc5fe2a5d6f65
Vladimir Lebedev <lvv1985@gmail.com>
2025-03-04 21:08:33 +0700

    add pytest and pytest-cov and 3 test for masks.py

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+
+
+@pytest.fixture
+def valid_card_number() -> str:
+    """Фикстура для валидного номера карты в виде строки."""
+    return "1234567890123456"
+
+
+@pytest.fixture
+def valid_card_number_int() -> int:
+    """Фикстура для валидного номера карты в виде целого числа."""
+    return 1234567890123456
+
+
+
---
+++ tests/test_masks.py
@@ -0,0 +1,24 @@
+from unittest import expectedFailure
+
+import pytest
+from typing import Optional, Union, Any
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def test_get_mask_card_number_with_valid_str(valid_card_number:str) -> None:
+    """Тест с валидным строковым номером карты."""
+    expected = '1234 56** **** 3456'
+    result = get_mask_card_number(valid_card_number)
+    assert result == expected
+
+def test_get_mask_card_number_with_valid_int(valid_card_number_int:int) -> None:
+    """Тест с валидным челочисленным номером карты"""
+    expected = '1234 56** **** 3456'
+    result = get_mask_card_number(valid_card_number_int)
+    assert result == expected
+
+def test_get_mask_card_number_with_none() -> None:
+    """Тест с валидным челочисленным номером карты"""
+    expected = None
+    result = get_mask_card_number(valid_card_number_int)
+    assert result == expected
