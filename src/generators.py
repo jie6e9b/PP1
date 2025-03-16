9c491145bf28fd89e2a2101c3a595b8c092baef8
Vladimir Lebedev <lvv1985@gmail.com>
2025-03-16 14:58:21 +0700

    flake8 correct errors

--- src/generators.py
+++ src/generators.py
@@ -1,46 +1,47 @@
-from typing import List, Dict, Iterator, Iterable
+from typing import List, Dict, Iterable
 
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
-    }
-]
+# transactions = [
+#     {
+#         "id": 939719570,
+#         "state": "EXECUTED",
+#         "date": "2018-06-30T02:08:58.425572",
+#         "operationAmount": {
+#             "amount": "9824.07",
+#             "currency": {
+#                 "name": "USD",
+#                 "code": "USD"
+#             }
+#         },
+#         "description": "Перевод организации",
+#         "from": "Счет 75106830613657916952",
+#         "to": "Счет 11776614605963066702"
+#     },
+#     {
+#         "id": 142264268,
+#         "state": "EXECUTED",
+#         "date": "2019-04-04T23:20:05.206878",
+#         "operationAmount": {
+#             "amount": "79114.93",
+#             "currency": {
+#                 "name": "USD",
+#                 "code": "USD"
+#             }
+#         },
+#         "description": "Перевод со счета на счет",
+#         "from": "Счет 19708645243227258542",
+#         "to": "Счет 75651667383060284188"
+#     }
+# ]
+
 
 def filter_by_currency(transactions: List[Dict], currency: str) -> Iterable[Dict]:
     """ Фильтрует список транзакций по указанной валюте.
     Args: transactions: Список словарей, представляющих транзакции.
-          currency: Строка с кодом валюты для фильтрации (например, "USD").
+    currency: Строка с кодом валюты для фильтрации (например, "USD").
     Returns: Итератор, который последовательно выдает транзакции с указанной валютой.
     Example: >>> usd_transactions = filter_by_currency(transactions, "USD")  >>> transaction = next(usd_transactions)
     Note: Валюта находится во вложенной структуре:
-        transaction["operationAmount"]["currency"]["code"] """
+    transaction["operationAmount"]["currency"]["code"] """
 
     return filter(lambda transaction: transaction["operationAmount"]["currency"]["code"] == currency, transactions)
 
@@ -50,19 +51,21 @@ def filter_by_currency(transactions: List[Dict], currency: str) -> Iterable[Dict
 # for i in range(2):
 #     print(next(usd_transactions))
 
+
 def transaction_descriptions(transactions: List[Dict]) -> Iterable[str]:
-        """ Генератор, который возвращает описания транзакций по очереди.
-        Args: transactions: список словарей с транзакциями
-        Yields: строка с описанием каждой транзакции
-        """
-        for transaction in transactions:
-            yield transaction.get("description", "Описание отсутствует")
+    """ Генератор, который возвращает описания транзакций по очереди.
+    Args: transactions: список словарей с транзакциями
+    Yields: строка с описанием каждой транзакции
+    """
+    for transaction in transactions:
+        yield transaction.get("description", "Описание отсутствует")
 
 # # Проверим работу фильтра генератора
 # descriptions = transaction_descriptions(transactions)
 # # Выводим результаты
 # for i in range(2):
 #     print(next(descriptions))
+
 
 def card_number_generator(start: int, end: int) -> Iterable[str]:
     """ Генератор номеров банковских карт в формате XXXX XXXX XXXX XXXX.
@@ -78,7 +81,7 @@ def card_number_generator(start: int, end: int) -> Iterable[str]:
 
     # Генерация номеров карт
     for number in range(start, end + 1):
-    # Преобразуем число в строку длинной 16 символов (добавляем нужное количесво нолей в начале)
+        # Преобразуем число в строку длинной 16 символов (добавляем нужное количесво нолей в начале)
         card_digits = str(number).zfill(16)
         card_correct = ' '.join([
             card_digits[0:4],
--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,51 @@ def valid_card_number() -> str:
 def valid_card_number_int() -> int:
     """Фикстура для валидного номера карты в виде целого числа."""
     return 1234567890123456
+
+
+@pytest.fixture
+def transaction_data():
+    """Фикстура, возвращающая тестовые данные транзакций"""
+    return [{
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }, {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    }, {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "RUB",
+                "code": "RUB"
+            }
+        },
+        "description": "Рублевый перевод",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    }]
